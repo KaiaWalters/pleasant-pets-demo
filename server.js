a924e56afca371769f70289cda238241874185f0
KaiaWalters <kaia.cwalters@gmail.com>
2022-02-09 09:50:11 -0500

    card styles are the same, will be changing full layout

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv'
 import database from './config/database.js'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
-import { ObjectId } from 'mongodb'
 import express from 'express'
 var app = express()
 app.use(express.static('public'));
@@ -18,40 +17,32 @@ app.use(bodyParser.json())
 dotenv.config()
 
 mongoose.connect(url, (err, database) => {
-  if (err) return console.log(err)
+  if (err) return console.log("I AM AN ERROR", err)
+  console.log("CREATING DB")
   db = database
 }); 
+mongoose.connection.on('connected', () => console.log('Connected'));
+mongoose.connection.on('error', () => console.log('Connection failed with - ',err));
 
-//
-let wasFetchCalled = true
+//in order to populate the db with images, you will need to set LOAD_MORE_IMAGES to true.
+//After you have the set amount of images you want to be voted on, set the env back
+// if(process.env.LOAD_MORE_IMAGES) {
+//   getImages()
+// }
 
 app.get("/", (req, res) => {
-  console.log("server started")
-
-  if(wasFetchCalled) {
+    console.log("server started") 
     console.log("queuing up cards to display")
     queueImages()
     .then( results => {
+      results.sort((a,b) => {
+        let first = a.upVotes - a.downVotes
+        let second = b.upVotes - b.downVotes
+        return second - first
+      })
       res.render("index", {array: results})
     })
-  
     console.log("RENDERING COMPLETE")
-  } 
-  else {
-    fetch(`https://api.thecatapi.com/v1/images/search?limit=${process.env.LIMIT}`)
-    .then(res => res.json())
-    .then(response => {
-      let urls = response.map(obj => {
-          return obj.url
-      })
-      let imageObj = response.map(obj => {
-        return {url: obj.url}
-    })
-      console.log("SAVING IMAGES")
-      saveNewCards(urls)
-      res.render("index", { array: imageObj})
-    })
-  }
 })
 
 app.put('/cards', (req, res) => {
@@ -73,13 +64,20 @@ app.put('/cards', (req, res) => {
       }, (err, result) => {
           console.log(err,result)
           return {err, result}
-      }).then(
-        //rerender the page after complete
-        //front end events should not be updated by backend results 
-        //A: investigate dynamic updating 
-        //B: return new page  
-      )
+      })
   })
+
+  function getImages() {
+    fetch(`https://api.thecatapi.com/v1/images/search?limit=${process.env.LIMIT}`)
+    .then(res => res.json())
+    .then(response => {
+      let urls = response.map(obj => {
+          return obj.url
+      })
+      console.log("SAVING IMAGES")
+      saveNewCards(urls)
+    })
+  }
 
   async function queueImages() {
     let images = []
@@ -88,16 +86,15 @@ app.put('/cards', (req, res) => {
     .forEach(function(item){
         images.push({url: item.url,upVotes: item.upVotes, downVotes: item.downVotes})
     })
+    
     return images
    }
 
    function saveNewCards(imageArray) {
-    wasFetchCalled = true 
     let count = 0
     let documents = imageArray.map( url => {
       return { url: url, upVotes: 0, downVotes: 0 }
     })
-
     db.collection('cards')
     .insertMany(
       documents,
